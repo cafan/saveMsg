a10214b41a91dd74d0f327f35fde380fcbf31bdf
cafan <1737956310@qq.com>
2023-04-15 14:14:46 +0800

    add caller func

--- saveMsg.go
+++ saveMsg.go
@@ -2,11 +2,11 @@ package saveMsg
 import(
 	"os"
 	"fmt"
+	"runtime"
 )
 type SaveMsg struct{
 	Path string 
-	Msg string 
-	CallerFuncName string 
+	CallerFuncName string
 }
 func NewSaveMsg(path string)*SaveMsg{
 	if path==""{
@@ -15,11 +15,26 @@ func NewSaveMsg(path string)*SaveMsg{
 	return &SaveMsg{Path:path}
 	
 }
-func (smg *SaveMsg)SaveMsgInPath(outputMsg string){
+func GetCallerInfo()(string,string){
+	pc,_,_,_:=runtime.Caller(1)
+	callerFunc:=runtime.FuncForPC(pc)
+	callerFile,_:=callerFunc.FileLine(pc)
+	callerName:=callerFunc.Name()
+	return callerFile,callerName
+}
+// SaveMsgInPath is aim to save the message in the pointed path
+// 1. get the caller function Name if setted before
+// 2. saved information format :
+//		<function name>:<key>:<value>
+func (smg *SaveMsg)SaveMsgInPath(outputMsg,callerFile,callerName string){
 	file,err:=os.OpenFile(smg.Path,os.O_APPEND|os.O_CREATE|os.O_WRONLY,0666)
+	saveStr:=outputMsg
 	defer file.Close()
 	if err!=nil{
 		fmt.Println("open file err=",err)
 	}
-	fmt.Fprintln(file,outputMsg)
+	if callerName!=""&&callerFile!=""{
+		saveStr=fmt.Sprintf("Called from %s: by function %s.\nWith parameter: %s",callerFile,callerName,saveStr)
+	}
+	fmt.Fprintln(file,saveStr)
 }
